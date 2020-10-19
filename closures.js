68599e6f399c211e27c529db0e45ed6ebe6a2bee
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-10-19 15:00:23 -0700

    added some answers

--- closures.js
+++ closures.js
@@ -51,7 +51,10 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+let callJake = callFriend('Jake');
+
+callJake(435-555-9248)
+
 
 
 
@@ -61,16 +64,22 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter(){
+  var num = 0;
+  function count(){
+    num ++;
+    return num
+  } return count
+}
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -86,18 +95,23 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
+  let counter = value;
+    return {
+      inc(){
+      return counter++;
+    },
+    dec(){
+      return counter--;
+    }
+  }
+};
 
-  return {
-
-  };
-}
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -112,10 +126,12 @@ counter = counterFactory(10);
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
-  // code message function here.
+  function message (){
+    return welcomeText + `${firstname} ${lastname}.`
+  }
 
   //Uncommment this to return the value of your message function
-  //return message;
+ return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -143,7 +159,9 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    function publicMethod(){
+
+    }
   };
 })();
 
@@ -191,6 +209,6 @@ function timeOutCounter() {
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
-  }
+  } 
 }
 timeOutCounter();
